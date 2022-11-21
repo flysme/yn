c237679b3b1a1fda00d21d7c36e530a19d02500b
zhaofeixiang <zhaofeixiang@myai.tech>
2022-11-21 20:56:36 +0800

    11

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,14 @@ export default defineConfig({
   base: './',
   server: {
     host: '0.0.0.0',
-    port: 3003
+    port: 3003,
+    proxy: {
+      '/proxy_img': {
+        target: 'https://m1.im5i.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/proxy_img/, '')
+      },
+    }
   },
   resolve: {
     alias: {
