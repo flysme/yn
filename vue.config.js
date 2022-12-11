ec6c99b049ea445d437ff93d461831592a66c687
zhaofeixiang <zhaofeixiang@myai.tech>
2022-12-11 22:35:49 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 // vue.config.js
 const path = require('path')
-const Components = require('unplugin-vue-components/webpack')
-const { VarletUIResolver } = require('unplugin-vue-components/resolvers')
+// const Components = require('unplugin-vue-components/webpack')
+// const { VarletUIResolver } = require('unplugin-vue-components/resolvers')
+// 导入compression-webpack-plugin
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 定义压缩文件类型
+const productionGzipExtensions = ['js', 'css', 'png']
 module.exports = {
   // 选项
   devServer: {
@@ -21,16 +25,24 @@ module.exports = {
   chainWebpack: (config) => {
     config.resolve.alias.set('@', path.join(__dirname, 'src'))
   },
-  configureWebpack: {
-    plugins: [
-      // Components({
-      //   resolvers: [
-      //     VarletUIResolver({
-      //       version: 'vue2',
-      //     }),
-      //   ],
-      //   directives: true,
+  productionSourceMap: false,
+  configureWebpack: (config) => {
+    //直接修改配置
+    const _plugins = [
+      // new CompressionWebpackPlugin({
+      //   asset: '[path].gz[query]', // 提示 compression-webpack-plugin@3.0.0的话asset改为filename
+      //   algorithm: 'gzip',
+      //   test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+      //   threshold: 10240,
+      //   minRatio: 0.8,
       // }),
-    ],
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ]
+    config.plugins = [...config.plugins, ..._plugins]
   },
 }
